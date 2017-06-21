0c6eca92ee4fbdf8530e186bcbe39d5e50d7a85c
since860321 <since860321@naver.com>
2017-06-21 22:31:28 +0900

    [Server] 빌드에러 수정

--- TCP_SkyrimServer_ProjectFile/cAI.h
+++ TCP_SkyrimServer_ProjectFile/cAI.h
@@ -1,16 +1,17 @@
 //#pragma once
 
 //몬스터 AI 용 클래스 싱글톤 : 케릭터 정보 포인터를 가지고 몬스터정보 포인터를 받는다
+
+#include "stdafx.h"
+
 class cAIStatus;
 class cActiveObject;
 class cCharacter;
-struct stClientInfo;
 /*
 	std::map<int, stClientInfo>::iterator iter = m_Client.begin();
 
 	for( iter = m_Client.begin() ; iter != m_Client.end(); iter ++ )
 */
-#include <map>
 
 class cAI
 {
