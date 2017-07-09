3f0cf90005760e2b52526100585f016cd502670a
since860321 <since860321@naver.com>
2017-07-09 15:54:46 +0900

    [Server] linux 빌드에러 수정 : linux에서 지원하지 않는 자료형 선언

--- TCP_SkyrimServer_ProjectFile/defines.h
+++ TCP_SkyrimServer_ProjectFile/defines.h
@@ -1,5 +1,12 @@
 #pragma once
 
+#ifdef _WIN32
+#else
+typedef unsigned long DWORD;
+typedef unsigned short WORD;
+typedef unsigned char  BYTE;
+#endif
+
 #include "macros.h"
 #include "constants.h"
 #include "structures.h"
