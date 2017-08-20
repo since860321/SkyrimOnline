059cc7ea6a6c7e0da10075dc25ee8d0753dfdf33
since860321 <since860321@naver.com>
2017-08-20 17:58:02 +0900

    [Server] 리눅스 빌드에러 수정(임시)

--- TCP_SkyrimServer_ProjectFile/cMove_Chase.cpp
+++ TCP_SkyrimServer_ProjectFile/cMove_Chase.cpp
@@ -15,22 +15,24 @@ void cMove_Chase::Process( stEnemyInfo* targetMonster )
 	vector vMoster	=	targetMonster->vPos;
 	vector vPos = vTarget - vMoster;
 	vector::normalize(&vPos,&vPos);
+
+	// 코드 추가해야함	
 #else //_S_MOD_D3DX9_API_CUSTOM_
 	D3DXVECTOR3 vTarget	=	targetMonster->pTarget->transformInfo.vPos;
 	D3DXVECTOR3 vMoster	=	targetMonster->vPos;
 	D3DXVECTOR3 vPos = vTarget - vMoster;
 	D3DXVec3Normalize(&vPos,&vPos);
-#endif //_S_MOD_D3DX9_API_CUSTOM_
 
 	targetMonster->vTargetPos	=	vTarget;
 	targetMonster->vPos += vPos * targetMonster->fMoveSpeed ;
-	
+
 	if(targetMonster->vPos.y < 100)
 	{
-	targetMonster->nAnimationIndex = (int)D_mtforwardground;
+		targetMonster->nAnimationIndex = (int)D_mtforwardground;
 	}
 	else
 	{
-	targetMonster->nAnimationIndex = (int)D_mtfastforward_flap;
+		targetMonster->nAnimationIndex = (int)D_mtfastforward_flap;
 	}
+#endif //_S_MOD_D3DX9_API_CUSTOM_
 }
